cd72a3587ef93ef41aeda971d7558f86346c5590
Seven Chord <hjlcarl@163.com>
2023-11-26 10:38:26 +0800

    dialog

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
 import '@/style/main.scss';
+
+async function test() {
+  const res = await fetch('/appearance/themes/Rem Craft Test/config.json');
+  const config = await res.json();
+  console.log(config);
+}
+await test();
